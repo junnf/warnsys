60990461e656f715cbf0900894188fa171078248
junn <junnflow@gmail.com>
2015-10-15 13:27:40 +0800

    edit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 #
+
 # -*- coding: utf-8 -*-
+
 #from string import Template
 #
-import temstand
 import json
 #
 import pymongo
@@ -13,20 +14,29 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
+import sendcloud.sendemail, sendcloud.sendemail
+
 from tornado.options import define, options
+
+#import sendcloud.sendemail
+from sendcloud import sendemail
+import sendcloud.sendmessage
+
+
 define("port", default=8000,help="nothing",type=int)
 
 class Application(tornado.web.Application):
     def __init__(self):
       
         handlers = [
-      (r"/api", ApiHandler),
-      (r"/api/condition", ConditionHandler),
-      (r"/api/conditionpeople/source", ConditionPeopleHandler),
-      (r"/test",TestHandler)
+            #      (r"/api", ApiHandler),
+            #      (r"/api/condition", ConditionHandler),
+            #      (r"/api/conditionpeople/source", ConditionPeopleHandler),
+            #      (r"/test",TestHandler)
+            (r"/api/post",  TongzhiHandler),
       ]
         conn = pymongo.Connection("localhost", 27017)
-        self.db = conn["warn_set"]
+        self.db = conn["member"]
         tornado.web.Application.__init__(self, handlers, debug=True)
 
 class TestHandler(tornado.web.RequestHandler):
@@ -59,10 +69,42 @@ class ApiHandler(tornado.web.RequestHandler):
        
 class NotifyHandler(tornado.web.RequestHandler):
     def post(self):
-        _condition = self.get_argument('condition')
+        '''_condition = self.get_argument('cond')
+        coll = self.application.db.warn_set
+        _notify = coll.find_one({'con_nam':_condition})
+        coll1 = self.application.db[_notify]
+        #pass message
+        _content = _notify['con_des']
+        for _item in coll1.find():
         #(email,mobi) from mongodb get
         pass
         #test api
+        '''
+
+
+class TongzhiHandler(tornado.web.RequestHandler):
+    def post(self):
+        '''
+        {
+          name:
+          email:
+          mobi:
+          }
+
+        '''
+        _cond = self.get_argument('cond')
+        _cond_des = self.get_argument('cond_des')
+        coll = self.application.db['member']
+        for _tem_meb in coll.find():
+          #notify
+            _pos_par = sendemail.get_params(emailto = str(_tem_meb['email']),
+                emailfrom = 'error_alert@sendcloud.org',
+                fromname = 'AlertCenter',
+                subject = _cond,
+                content = _cond_des 
+                )
+            sendemail.sendemail(sendemail.url,_pos_par)
+
 
 class ConditionHandler(tornado.web.RequestHandler):
   
@@ -72,11 +114,16 @@ class ConditionHandler(tornado.web.RequestHandler):
         #warn_set
         coll = self.application.db.warn_set
         #elems_coll = coll.find({},{"condition_name" : 1,"_id" : 0})
-        condition_list = coll.distinct('condition_name') 
+        condition_list = coll.distinct('con_nam') 
         self.write(json.dumps(condition_list))
    
     def post(self):
-        _condition_name = self.get_argument('condition_name')
+        ''' 
+          condition_name = con_nam
+          condition_description = con_des
+        '''
+        _condition_name = self.get_argument('con_nam')
+        _condition_description = self.get_argument('con_des')
         #test
         #print (_condition_name == u"")
         #print _condition_name
@@ -84,26 +131,30 @@ class ConditionHandler(tornado.web.RequestHandler):
         _tem_dict = {}
         if _condition_name != u'':
             coll = self.application.db.warn_set    
-            if coll.find({ 'condition_name' : _condition_name }) == None:
-                _tem_dict['condition_name'] = _condition_name
+            if coll.find({ 'con_nam' : _condition_name }) == None:
+                _tem_dict['con_nam'] = _condition_name
+                _tem_dict['con_des'] = _condition_description
                 coll.insert(_tem_dict)
        
 
 class ConditionPeopleHandler(tornado.web.RequestHandler):
     
-    '''(r"/api/conditionpeople/source", ConditionPeopleHandler)'''
+    '''
+        (r"/api/conditionpeople/source", ConditionPeopleHandler)
+        use api /api/conditionpeople/source?condname=
+    '''
     
     def get(self):
         
-        _cond_name = self.get_argument('cond_name')
+        _cond_name = self.get_argument('condname')
         _tem_people_dict = {}
         if _cond_name != u'':
             coll = self.application.db[_cond_name]
             for x in coll.find():
                 _tem_people_dict[x[u'name']] = {
-                    'name':x['name'],
-                    'email':x['email'],
-                    'mobi':x['mobi']
+                    'name' : x['name'],
+                    'email' : x['email'],
+                    'mobi' : x['mobi']
                     }
                 self.write(json.dumps(_tem_people_dict))
 
@@ -113,7 +164,7 @@ class ConditionPeopleHandler(tornado.web.RequestHandler):
     def post(self):
         
         #url
-        _cond_name_set = self.get_argument('condition')
+        _cond_name_set = self.get_argument('cond')
         _person = self.get_argument('person')
         _email = self.get_argument('email')
         _mobi = self.get_argument('mobi')
